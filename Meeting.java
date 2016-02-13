62fd747c8ca3982a8e481e762fdb7ec2b725ce27
janss001 <janss001@gmail.com>
2016-02-13 15:24:25 +0000

    Added getters to interface

--- Meeting.java
+++ Meeting.java
@@ -8,4 +8,28 @@ import java.util.Set;
 */
 public interface Meeting {
 
+	/**
+	 * Returns the id of the meeting.
+	 *
+	 * @return the id of the meeting.
+	 */
+	int getId();
+	
+	/**
+	 * Return the date of the meeting.
+	 *
+	 * @return the date of the meeting.
+	 */
+	Calendar getDate();
+	
+	/**
+	 * Return the details of people that attended the meeting.
+	 *
+	 * The list contains a minimum of one contact (if there were
+	 * just two people: the user and the contact) and may contain an
+	 * arbitrary number of them.
+	 *
+	 * @return the details of people that attended the meeting.
+	 */
+	Set<Contact> getContacts();
 }
