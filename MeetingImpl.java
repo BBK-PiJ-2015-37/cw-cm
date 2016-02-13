f7acf044d547c93b888d469d290ca3ec8b35bab4
janss001 <janss001@gmail.com>
2016-02-13 15:17:26 +0000

    Initial commit of MeetingImpl, no implementation of constructor

---
+++ MeetingImpl.java
@@ -0,0 +1,8 @@
+import java.util.Calendar;
+import java.util.Set;
+
+public abstract class MeetingImpl implements Meeting {
+	
+	public MeetingImpl(int id, Calendar date, Set<Contact> attendees) {
+	}
+}
