cd1e55fcb97a6507454be696f202842bb879c18d
janss001 <janss001@gmail.com>
2016-02-13 15:21:11 +0000

    Implemented constructor

--- MeetingImpl.java
+++ MeetingImpl.java
@@ -2,7 +2,22 @@ import java.util.Calendar;
 import java.util.Set;
 
 public abstract class MeetingImpl implements Meeting {
+	private int id;
+	private Calendar date;
+	private Set<Contact> attendees;
 	
 	public MeetingImpl(int id, Calendar date, Set<Contact> attendees) {
+		if (id <= 0) {
+			throw new IllegalArgumentException("ID must be positive and non-zero");
+		}
+		if (attendees.isEmpty()) {
+			throw new IllegalArgumentException("List of attendees cannot be empty");
+		}
+		if (date.equals(null) || attendees.equals(null)) {
+			throw new NullPointerException("Null parameters not permitted");
+		}
+		this.id = id;
+		this.date = date;
+		this.attendees = attendees;
 	}
 }
