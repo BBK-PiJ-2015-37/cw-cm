ff1c27bc42dc9546f47b32cec5eae45030de6c4f
janss001 <janss001@gmail.com>
2016-02-13 14:38:32 +0000

    Initial commit of Meeting interface

---
+++ Meeting.java
@@ -0,0 +1,11 @@
+import java.util.Calendar;
+import java.util.Set;
+
+/**
+* A class to represent meetings
+*
+* Meetings have unique IDs, scheduled date and a list of participating contacts
+*/
+public interface Meeting {
+
+}
