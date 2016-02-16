2067941fea036c4aa505671978c829ad71b37e4d
janss001 <janss001@gmail.com>
2016-02-16 10:34:06 +0000

    Added tests for exceptions in PastMeeting constructor

---
+++ PastMeetingTests.java
@@ -0,0 +1,54 @@
+import java.util.GregorianCalendar;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.Test;
+import org.junit.Before;
+
+public class PastMeetingTests {
+	private Meeting m1;
+	private Set<Contact> attendees;
+	private Contact c1;
+	private Contact c2;
+	private Calendar date;
+	private String notes;
+	
+	@Before
+	public void setUp() {
+		c1 = new ContactImpl(1, "John", "Likes cats");
+		c2 = new ContactImpl(2, "Carol");
+		date = new GregorianCalendar(2015, 2, 10);
+		notes = "Good meeting";
+		attendees = new HashSet<>();
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testsNegativeIDThrowsException() {
+		attendees.add(c1);
+		m1 = new PastMeetingImpl(-1, date, attendees, notes);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testsEmptyContactSetThrowsException() {
+		m1 = new PastMeetingImpl(1, date, attendees, notes);
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testsNullDateThrowsException() {
+		attendees.add(c1);
+		date = null;
+		m1 = new PastMeetingImpl(1, date, attendees, notes);
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testsNullContactSetThrowsException() {
+		attendees = null;
+		m1 = new PastMeetingImpl(1, date, attendees, notes);
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testsNullNotesStringThrowsException() {
+		notes = null;
+		m1 = new PastMeetingImpl(1, date, attendees, notes);
+	}
+}
