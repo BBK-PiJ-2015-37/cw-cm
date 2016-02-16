35f1407a97713aaa343b1267a769d8f1b8d5f214
janss001 <janss001@gmail.com>
2016-02-16 09:57:34 +0000

    Adding tests for exceptions in FutureMeeting constructor; note: commits eec4cb8 to 7e96e61 should show following progression -- implementing getters for MeetingImpl, adding JavaDoc to MeetingImpl constructor, then adding FutureMeeting interface

---
+++ FutureMeetingTests.java
@@ -0,0 +1,46 @@
+import java.util.GregorianCalendar;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.Test;
+import org.junit.Before;
+
+public class FutureMeetingTests {
+	private Meeting m1;
+	private Set<Contact> attendees;
+	private Contact c1;
+	private Contact c2;
+	private Calendar date;
+	
+	@Before
+	public void setUp() {
+		c1 = new ContactImpl(1, "John", "Likes cats");
+		c2 = new ContactImpl(2, "Carol");
+		date = new GregorianCalendar(2017, 2, 20);
+		attendees = new HashSet<>();
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testsNegativeIDThrowsException() {
+		attendees.add(c1);
+		m1 = new FutureMeetingImpl(-1, date, attendees);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testsEmptyContactSetThrowsException() {
+		m1 = new FutureMeetingImpl(1, date, attendees);
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testsNullDateThrowsException() {
+		attendees.add(c1);
+		date = null;
+		m1 = new FutureMeetingImpl(1, date, attendees);
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testsNullContactSetThrowsException() {
+		attendees = null;
+		m1 = new FutureMeetingImpl(1, date, attendees);
+	}
+}
