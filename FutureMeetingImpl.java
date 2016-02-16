9530637ff00453de897f27fb0d768b1d493b81c3
janss001 <janss001@gmail.com>
2016-02-16 10:10:10 +0000

    Implemented FutureMeetingImpl constructor, added constructor JavaDoc

--- FutureMeetingImpl.java
+++ FutureMeetingImpl.java
@@ -1,8 +1,21 @@
 import java.util.Calendar;
 import java.util.Set;
 
-public class FutureMeetingImpl {
+public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
 	
+	/**
+	 * Constructs future meeting with id, date and a set of attendees
+	 * using constructor of MeetingImpl superclass.
+	 *
+	 * @param id Meeting's ID number
+	 * @param date Date of meeting
+	 * @param attendees Set of meeting's attendees
+	 *
+	 * @throws IllegalArgumentException if id is negative or zero
+	 *		   or if the set of attendees is empty
+	 * @throws NullPointerException if date or set of attendees is null
+	 */
 	public FutureMeetingImpl(int id, Calendar date, Set<Contact> attendees) {
+		super(id, date, attendees);
 	}
 }
