de4c3096e79fdb78e4209245d7ec5b9e05ac4050
janss001 <janss001@gmail.com>
2016-02-17 17:03:00 +0000

    Initial commit of ContactManagerImpl with addNewContact() method that fails all tests

---
+++ ContactManagerImpl.java
@@ -0,0 +1,13 @@
+import java.util.Calendar;
+import java.util.List;
+import java.util.Set;
+
+public class ContactManagerImpl implements ContactManager {
+	
+	/**
+	 * @see ContactManager
+	 */
+	public int addNewContact(String name, String notes) {
+		return -999;
+	}
+}
