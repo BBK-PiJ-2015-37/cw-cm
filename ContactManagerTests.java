b5423deb90dccf351e2e61b5f8de1f9f2aca3f52
janss001 <janss001@gmail.com>
2016-02-21 13:21:40 +0000

    Added tests for getContacts(String)

--- ContactManagerTests.java
+++ ContactManagerTests.java
@@ -1,7 +1,9 @@
+import java.util.Set;
 import org.junit.Test;
 import org.junit.Before;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ContactManagerTests {
 	ContactManager cm;
@@ -52,5 +54,70 @@ public class ContactManagerTests {
 	@Test (expected = NullPointerException.class)
 	public void testsAddNewContactWithNullNotesThrowsException() {
 		cm.addNewContact("John", null);
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testsGetContactsWithNullStringThrowsException() {
+		cm.getContacts(null);
+	}
+	
+	@Test
+	public void testsGetContactsWithEmptyStringReturnsFullContactSet() {
+		cm.addNewContact("John", "Likes cats");
+		cm.addNewContact("Carol", "Good at business");
+		cm.addNewContact("Derek", "Never wears a tie");
+		Set<Contact> output = cm.getContacts("");
+		assertTrue(output.size() == 3);
+	}
+	
+	@Test
+	public void testsGetContactsWithStringOnEmptyContactSetReturnsEmptySet() {
+		Set<Contact> output = cm.getContacts("John");
+		assertTrue(output.isEmpty());
+	}
+	
+	@Test
+	public void testsGetContactsWithEmptyStringOnEmptyContactSetReturnsEmptySet() {
+		Set<Contact> output = cm.getContacts("");
+		assertTrue(output.isEmpty());
+	}
+	
+	@Test
+	public void testsGetContactsWithStringReturnsEmptySetWhenNoMatches() {
+		cm.addNewContact("John", "Likes cats");
+		cm.addNewContact("Carol", "Good at business");
+		cm.addNewContact("Derek", "Never wears a tie");
+		Set<Contact> output = cm.getContacts("Francine");
+		assertTrue(output.isEmpty());
+	}
+	
+	@Test
+	public void testsGetContactsWithStringReturnsSetWithOneItemWhenOneMatch() {
+		cm.addNewContact("John", "Likes cats");
+		cm.addNewContact("Carol", "Good at business");
+		cm.addNewContact("Derek", "Never wears a tie");
+		Set<Contact> output = cm.getContacts("John");
+		assertTrue(output.size() == 1);
+		assertTrue(output.contains("John"));
+	}
+	
+	@Test
+	public void testsGetContactsWithStringReturnsSetWithTwoItemsWhenTwoMatches() {
+		cm.addNewContact("John A", "Likes cats");
+		cm.addNewContact("John B", "Good at business");
+		cm.addNewContact("Derek", "Never wears a tie");
+		Set<Contact> output = cm.getContacts("John");
+		assertTrue(output.size() == 2);
+		assertTrue(output.contains("John"));
+	}
+	
+	@Test
+	public void testsGetContactsWithStringIsCaseSensitive() {
+		cm.addNewContact("John", "Likes cats");
+		cm.addNewContact("Carol Notjohn", "Good at business");
+		cm.addNewContact("Derek", "Never wears a tie");
+		Set<Contact> output = cm.getContacts("John");
+		assertTrue(output.size() == 1);
+		assertTrue(output.contains("John"));
 	}
 }
