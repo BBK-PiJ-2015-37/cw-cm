a2150ba1aac20e8b7bf14a8be61f99bf28154380
janss001 <janss001@gmail.com>
2016-02-21 12:44:17 +0000

    Implemented addNewContact(); added basic ContactManagerImpl constructor that currently only aids functionality of above method, will develop further as class progresses

--- ContactManagerImpl.java
+++ ContactManagerImpl.java
@@ -1,13 +1,28 @@
 import java.util.Calendar;
 import java.util.List;
+import java.util.HashSet;
 import java.util.Set;
 
 public class ContactManagerImpl implements ContactManager {
+	private Set<Contact> contactList;
+	
+	public ContactManagerImpl() {
+		contactList = new HashSet<>();
+	}
 	
 	/**
 	 * @see ContactManager
 	 */
 	public int addNewContact(String name, String notes) {
-		return -999;
+		if (name.equals(null) || notes.equals(null)) {
+			throw new NullPointerException("Null parameters not permitted");
+		}
+		if (name.equals("") || notes.equals("")) {
+			throw new IllegalArgumentException("Empty strings not permitted");
+		}
+		int newContactId = contactList.size() + 1;
+		Contact newContact = new ContactImpl(newContactId, name, notes);
+		contactList.add(newContact);
+		return newContactId;
 	}
 }
