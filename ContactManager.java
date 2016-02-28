d8cf240f5f6389d68a759a8ee55fbbcc316f9457
janss001 <janss001@gmail.com>
2016-02-28 15:19:57 +0000

    Added getPastMeeting() to ContactManager interface

--- ContactManager.java
+++ ContactManager.java
@@ -22,6 +22,18 @@ public interface ContactManager {
 	int addFutureMeeting(Set<Contact> contacts, Calendar date);
 	
 	/**
+	 * Returns the PAST meeting with the requested ID, or null if it there is none.
+	 *
+	 * The meeting must have happened at a past date.
+	 *
+	 * @param id the ID for the meeting
+	 * @return the meeting with the requested ID, or null if it there is none.
+	 * @throws IllegalStateException if there is a meeting with that ID happening
+	 * in the future
+	 */
+	PastMeeting getPastMeeting(int id);
+	
+	/**
 	 * Returns the FUTURE meeting with the requested ID, or null if there is none.
 	 *
 	 * @param id the ID for the meeting
