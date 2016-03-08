9f3ea0f1ccced514127f9daec60ae185b1e8bacd
janss001 <janss001@gmail.com>
2016-03-08 17:25:04 +0000

    Initial commit, new class to hold some static methods to aid with ContactManager implementation

---
+++ ContactManagerUtils.java
@@ -0,0 +1,10 @@
+/*
+ * A class for several static methods useful in the implementation
+ * of ContactManager and various related classes.
+ *
+ * @author Sam Jansen
+ */
+
+public class ContactManagerUtils {
+	
+}
